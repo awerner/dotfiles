03c379b1572e18610428ce180e0a9f17f35a9a79
Alexander Werner <bundeswerner@googlemail.com>
2018-09-21 07:03:47 +0200

    Create zplug.zsh

---
+++ zplug.zsh
@@ -0,0 +1,10 @@
+zplug 'zplug/zplug', hook-build:'zplug --self-manage', defer:0
+zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:fzf, use:\*linux_amd64\*
+zplug "lib/completion", from:oh-my-zsh
+zplug "lib/git", from:oh-my-zsh
+zplug "lib/history", from:oh-my-zsh
+zplug "lib/key-bindings", from:oh-my-zsh
+zplug "denysdovhan/spaceship-zsh-theme", use:spaceship.zsh, from:github, as:theme
+zplug "junegunn/fzf", use:shell/key-bindings.zsh, defer:1
+zplug "zsh-users/zsh-syntax-highlighting", from:github, defer:3
+zplug "ogham/exa", from:gh-r, as:command, use:\*linux-x86\*, rename-to:exa
