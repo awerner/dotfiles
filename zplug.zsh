0bf843d0b0589d1d3f85ae07beaf07bfafe3709f
Alexander Werner <bundeswerner@googlemail.com>
2018-10-09 06:34:27 +0200

    Update zplug.zsh

--- zplug.zsh
+++ zplug.zsh
@@ -1,5 +1,5 @@
 zplug 'zplug/zplug', hook-build:'zplug --self-manage', defer:0
-zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:fzf, use:\*linux_amd64\*
+
 zplug "lib/completion", from:oh-my-zsh
 zplug "lib/git", from:oh-my-zsh
 zplug "lib/history", from:oh-my-zsh
@@ -7,4 +7,8 @@ zplug "lib/key-bindings", from:oh-my-zsh
 zplug "denysdovhan/spaceship-zsh-theme", use:spaceship.zsh, from:github, as:theme
 zplug "junegunn/fzf", use:shell/key-bindings.zsh, defer:1
 zplug "zsh-users/zsh-syntax-highlighting", from:github, defer:3
-zplug "ogham/exa", from:gh-r, as:command, use:\*linux-x86\*, rename-to:exa
+
+if [ `uname -s` = 'Linux' ]; then
+  zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:fzf, use:\*linux_amd64\*
+  zplug "ogham/exa", from:gh-r, as:command, use:\*linux-x86\*, rename-to:exa
+fi
